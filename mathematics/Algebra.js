8a33d03e42a458e2a03d9e2efd0b5c9b74f0f051
Marta Zaorska <marta.zaorska2@gmail.com>
2020-10-28 16:28:05 +0100

    Created methods symbolNewton and binomialTheorem in Algebra class

--- mathematics/Algebra.js
+++ mathematics/Algebra.js
@@ -17,6 +17,33 @@ class Algebra {
     this.#validateNaturalNumber(n);
     return this.#calcFactorial(n);
   }
+
+  static symbolNewton(n, k) {
+    this.#validateNaturalNumber(n,k);
+    if(n < k) throw new Error("");
+
+    const factorialN = this.#calcFactorial(n);
+    const factorialK = this.#calcFactorial(k);
+    const factorialNK = this.#calcFactorial(n - k);
+
+    return parseInt(factorialN / (factorialK * factorialNK));
+  }
+
+  static binomialTheorem(n){
+    this.#validateNaturalNumber(n);
+    let result = `(a+b)^${n} = a^${n} + `;
+
+    for(let k = 1; k < n; ++k){
+      const symbolNewton = this.symbolNewton(n, k);
+      result += `${symbolNewton}*`;
+      result += n -k === 1 ? 'a*' : `a^${n-k}*`;
+      result += k ===1 ? 'b + ' : `b^${k} + `;
+    }
+
+    result += `b^${n}`;
+
+    return result;
+  }
 }
 
 module.exports = Algebra;
