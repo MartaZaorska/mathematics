3248eb588ace1059034a8adb9d80b0e619e9711a
Marta Zaorska <marta.zaorska2@gmail.com>
2021-03-27 11:33:49 +0100

    Removed MathEquation module

--- mathematics/index.js
+++ mathematics/index.js
@@ -1,10 +1,8 @@
-const MathEquation = require('./MathEquation');
 const Vector = require('./Vector');
 const Algebra = require('./Algebra');
 const Matrix = require('./Matrix');
 
 module.exports = {
-  MathEquation,
   Vector,
   Algebra,
   Matrix,
